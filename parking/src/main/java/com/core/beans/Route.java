5ae15251f3bba4b0e4dc34c3feabf3e3fe3ea6ba
Kiryl Lutsyk <zetsubou.zero.0@gmail.com>
2014-08-31 14:28:40 +0300

    v2.1

--- parking/src/main/java/com/core/beans/Route.java
+++ parking/src/main/java/com/core/beans/Route.java
@@ -1,7 +1,10 @@
 package com.core.beans;
 
+import java.util.Map;
+
 /**
  * Created by Viachaslau_Karnausha on 8/31/2014.
  */
 public interface Route {
+    public Map<Integer, Integer> getRoutes();
 }
--- parking/src/main/java/com/test/JsonTest.java
+++ parking/src/main/java/com/test/JsonTest.java
@@ -4,6 +4,8 @@ import com.core.beans.Route;
 import com.core.beans.Transport;
 import org.json.JSONArray;
 
+import java.util.Map;
+
 /**
  * Created by zetsubou_0 on 31.8.14.
  */
@@ -13,7 +15,7 @@ public class JsonTest {
         String jsonString = "";
         JSONArray jsonArray = new JSONArray();
         Route route = transport.getRoute();
-        
+        Map<Integer, Integer> routes = route.getRoutes();
         return jsonString;
     }
 
