56167fdc1c7b81e137d351c3e02ef54f7cabff08
Kiryl Lutsyk <zetsubou.zero.0@gmail.com>
2014-08-31 14:36:12 +0300

    v2.3

---
+++ parking/src/main/java/com/core/beans/Point.java
@@ -0,0 +1,25 @@
+package com.core.beans;
+
+/**
+ * Created by zetsubou_0 on 31.8.14.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
