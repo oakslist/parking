08921fa23d434c95e0beb5f8d37f59195651d6f8
Viachaslau_Karnausha <viachaslau_karnaushanka@epam.com>
2014-08-31 13:01:59 +0300

    add beans

--- ParkingApp/src/main/java/com/core/impl/Car.java
+++ ParkingApp/src/main/java/com/core/impl/Car.java
@@ -13,4 +13,6 @@ public class Car extends Transport {
     public List<int[][]> move() {
         return null;
     }
+
+
 }
---
+++ ParkingApp/src/main/java/com/core/impl/EpamParking.java
@@ -0,0 +1,9 @@
+package com.core.impl;
+
+import com.core.beans.Parking;
+
+/**
+ * Created by Viachaslau_Karnausha on 8/31/2014.
+ */
+public class EpamParking extends Parking {
+}
