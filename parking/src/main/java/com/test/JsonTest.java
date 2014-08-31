9beaa4308d8678535a96b0a436d11f9221a2b32f
Kiryl Lutsyk <zetsubou.zero.0@gmail.com>
2014-08-31 14:23:55 +0300

    refactoring

--- parking/src/main/java/com/test/JsonTest.java
+++ parking/src/main/java/com/test/JsonTest.java
@@ -1,8 +1,20 @@
 package com.test;
 
+import com.core.beans.Route;
+import com.core.beans.Transport;
+import org.json.JSONArray;
+
 /**
  * Created by zetsubou_0 on 31.8.14.
  */
 public class JsonTest {
-    
+
+    public String getJSON(Transport transport) {
+        String jsonString = "";
+        JSONArray jsonArray = new JSONArray();
+        Route route = transport.getRoute();
+        
+        return jsonString;
+    }
+
 }
